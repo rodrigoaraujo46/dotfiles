3850f22b9ada11008d3c5bc889db080631995fd7
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-09-16 16:50:59 +0100

    Fixed go back in trouble.lua

--- .config/nvim/lua/animus/lazy/trouble.lua
+++ .config/nvim/lua/animus/lazy/trouble.lua
@@ -9,17 +9,16 @@ return {
             })
 
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle({mode = "diagnostics"})
+                require("trouble").toggle({ mode = "diagnostics" })
             end)
 
             vim.keymap.set("n", "]t", function()
-                require("trouble").next({mode = "diagnostics", skip_groups = true, jump = true});
+                require("trouble").next({ mode = "diagnostics", skip_groups = true, jump = true });
             end)
 
             vim.keymap.set("n", "[t", function()
-                require("trouble").previous({mode = "diagnostics", skip_groups = true, jump = true});
+                require("trouble").prev({ mode = "diagnostics", skip_groups = true, jump = true });
             end)
-
         end
     },
 }
