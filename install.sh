2e021d98d719b66674f4f67533a42ef20d2c4f64
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2026-04-18 23:01:42 +0100

    Add nbfc-linux and autocpu-freq

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ fail() {
 }
 
 setupPKGManagers() {
-	sudo pacman -S --noconfirm git base-devel
-	sudo pacman -S --asdeps --noconfirm cargo
+	sudo pacman -S --noconfirm git base-devel || fail "git/base-devel failed"
+	sudo pacman -S --asdeps --noconfirm cargo || fail "cargo install failed"
 
 	#disable debug
 	MAKEPKG_CONF="/etc/makepkg.conf"
@@ -23,72 +23,110 @@ setupPKGManagers() {
 }
 
 installPackages() {
-	cat ./pacman.packages | sudo pacman -S --needed --noconfirm -
-	cat ./aur.packages | paru -S --needed -
+	cat ./pacman.packages | sudo pacman -S --needed --noconfirm - || fail "pacman packages failed"
+	cat ./aur.packages | paru -S --needed - || fail "aur packages failed"
 }
 
 stowDots() {
 	mkdir -p "$HOME/.local/bin/" "$XDG_CONFIG_HOME/opencode" "$XDG_CONFIG_HOME/tmux" "$XDG_CONFIG_HOME/zen"
 	# shellcheck disable=2035
-	stow */
+	stow */ || fail "stow failed"
 
-	chsh -s "$(which zsh)"
+	chsh -s "$(which zsh)" || fail "chsh failed"
 }
 
 setupDrivers() {
 	printf "Do you want to install NVIDIA open drivers? (y/N): "
 	read -r install_choice
 	case "$install_choice" in
-	[yY][eE][sS] | [yY])
-		echo "Installing nvidia-open..." sudo pacman -S --needed nvidia-open
+	[yY]*)
+		echo "Installing nvidia-open..."
+		sudo pacman -S --needed nvidia-open || echo "Warning: nvidia-open install failed"
+
+		printf "Do you want to configure hybrid graphics based on graphics.sh script? (y/N): "
+		read -r hybrid_choice
+
+		case "$hybrid_choice" in
+		[yY]*)
+			# shellcheck disable=1091
+			. ./graphics.sh || echo "Warning: graphics.sh failed"
+
+			echo ""
+			echo "-----------------------------------------------------"
+			echo "Verify export AQ_DRM_DEVICES"
+			echo "Should be exported based on graphics.sh success"
+			echo "File path: $XDG_CONFIG_HOME/uwsm/env-hyprland"
+			echo "-----------------------------------------------------"
+			;;
+		*)
+			echo "Hybrid configuration skipped."
+			;;
+		esac
 		;;
 	*)
 		echo "Skipping Driver installation."
-		return
 		;;
 	esac
+}
 
-	printf "Do you want to configure hybrid graphics based on graphics.sh script? (y/N): "
-	read -r hybrid_choice
-
-	case "$hybrid_choice" in
-	[yY][eE][sS] | [yY])
-		# shellcheck disable=1091
-		. ./graphics.sh
-		;;
-	*)
-		echo "Hybrid configuration skipped."
-		;;
-	esac
-
-	echo ""
-	echo "-----------------------------------------------------"
-	echo "Verify export AQ_DRM_DEVICES"
-	echo "Should be exported based on graphics.sh success"
-	echo "File path: $XDG_CONFIG_HOME/uwsm/env-hyprland"
-	echo "-----------------------------------------------------"
+nbfc_setup() {
+	if ! sudo nbfc config --set auto; then
+		echo "Warning: couldn't set nbfc config"
+		return 0
+	fi
+	if ! sudo nbfc restart; then
+		echo "Warning: couldn't restart nbfc service"
+		return 0
+	fi
+	if ! sudo nbfc --auto; then
+		echo "Warning: couldn't set nbfc to auto"
+		return 0
+	fi
+	if ! sudo systemctl enable nbfc_service; then
+		echo "Warning: couldn't enable nbfc_service"
+	fi
 }
 
 handleAppSetup() {
 	#WALKER
-	elephant service enable
+	elephant service enable || echo "Warning: elephant failed"
 
 	#TMUX
 	mkdir -p "$XDG_CONFIG_HOME/tmux/plugins/"
 	{
 		git clone https://github.com/tmux-plugins/tpm "$XDG_CONFIG_HOME/tmux/plugins/tpm" &&
 			"$XDG_CONFIG_HOME/tmux/plugins/tpm/scripts/install_plugins.sh"
-	} || fail "Couldn't clone tmux tpm"
+	} || echo "Warning: tmux tpm failed"
 
 	#SPOTIFY
-	spicetify config spotify_path "$XDG_DATA_HOME/spotify-launcher/install/usr/share/spotify"
-	mkdir -p "$XDG_CONFIG_HOME/spicetify/Themes/"
-	git clone --depth=1 https://github.com/spicetify/spicetify-themes.git || fail "Failed to clone spicetify-themes.git"
-	cp -r ./spicetify-themes/* "$XDG_CONFIG_HOME/spicetify/Themes"
-	rm -rf ./spicetify-themes
+	if command -v spicetify &>/dev/null; then
+		spicetify config spotify_path "$XDG_DATA_HOME/spotify-launcher/install/usr/share/spotify" 2>/dev/null || true
+		mkdir -p "$XDG_CONFIG_HOME/spicetify/Themes/"
+		{
+			git clone --depth=1 https://github.com/spicetify/spicetify-themes.git &&
+				cp -r ./spicetify-themes/* "$XDG_CONFIG_HOME/spicetify/Themes" &&
+				rm -rf ./spicetify-themes
+		} || echo "Warning: spicetify themes failed"
+	else
+		echo "Warning: spicetify not installed, skipping"
+	fi
 
 	#BAT
-	bat cache --build
+	if command -v bat &>/dev/null; then
+		bat cache --build || echo "Warning: bat cache failed"
+	else
+		echo "Warning: bat not installed, skipping"
+	fi
+
+	#NBFC-LINUX
+	nbfc_setup
+
+	#CPUFREQ
+	if command -v auto-cpufreq &>/dev/null; then
+		sudo auto-cpufreq --install || echo "Warning: auto-cpufreq install failed"
+	else
+		echo "Warning: auto-cpufreq not installed, skipping"
+	fi
 }
 
 promptReboot() {
